7e498d13e2959828ace8b9d3880d4fb25f1328e8
Harsh <harsh0705singh@gmail.com>
2026-02-15 11:50:11 +0530

    added more test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,16 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add_funcation():
     assert add(5, 3) == 8
     assert add(6, 4) == 10
-    assert add(5, 4) == 10
+    assert add(5, 4) == 9
+
+def test_subtract_funcation():
+    assert subtract(5, 3) == 2
+    assert subtract(6, 4) == 2
+    assert subtract(5, 4) == 1
+
+
+
 
 
